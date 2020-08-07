1d19f7d65c4f7325f444b35eb1c43de1330f01fb
Tomasz Sieczko <sieczko.tomasz@gmail.com>
2020-08-07 15:28:45 -0700

    Chapter10

---
+++ csharp_8_and_dotnet_core_3/Chapter10/Chapter10.md
@@ -0,0 +1,41 @@
+# Exercise 10.1
+
+1. Of the encryption algorithms provided by .NET, which is the best choice for symmetric encryption?
+
+AES.
+
+2. Of the encryption algorithms provided by .NET, which is the best choice for asymmetric encryption?
+
+RSA
+
+3. What is a rainbow attack?
+
+Looking up pre-computed hashes in large tables to find the original password.
+
+4. For encryption algorithms, is it better to have a larger or smaller block size?
+
+Larger block size is better because there is a reduced chance of duplicate blocks.
+
+5. What is a hash?
+
+A one way encryption.
+
+6. What is a signature?
+
+A collection of data signed with the private key.
+
+7. What is the difference between symmetric and asymmetric encryption?
+
+Symmetric uses the same key to encrypt and decrypt while asymmetric uses a pair of public and private keys to encrypt decrypt.
+
+8. What does RSA stand for?
+
+Rivest Shamit Adleman.
+
+9. Why should passwords be salted before being stored?
+
+To prevent duplicate passwords being stored with the same hash.
+
+10. SHA1 is a hashing algorithm designed by the United States National Security Agency. Why should you never use it?
+
+The hashes can be easily cracked with modern hardware.
---
+++ csharp_8_and_dotnet_core_3/Chapter10/EncryptionApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography;
+using CryptographyLib;
+using static System.Console;
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter a message that you want ot encrypt: ");
+            string message = ReadLine();
+            Write("Enter a password: ");
+            string password = ReadLine();
+
+            string cryptoText = Protector.Encrypt(message, password);
+
+            WriteLine($"Encrypted text: {cryptoText}");
+            Write("Enter the password: ");
+            string password2 = ReadLine();
+
+            try
+            {
+                string clearText = Protector.Decrypt(cryptoText, password2);
+                WriteLine($"Decrypted text: {clearText}");
+            }
+            catch (CryptographicException ex)
+            {
+                WriteLine(
+                    "{0}\nMore details: {1}",
+                    "You entered the wrong password",
+                    ex.Message
+                );
+            }
+            catch (Exception ex)
+            {
+                WriteLine(
+                    "Non-cryptographic exception: {0}, {1}",
+                    ex.GetType().Name,
+                    ex.Message
+                );
+            }
+        }
+    }
+}
