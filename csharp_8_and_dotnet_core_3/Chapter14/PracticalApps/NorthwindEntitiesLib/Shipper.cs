64dbc0091f64f2d517d1a31ba8ab9768ad85086d
Tomasz Sieczko <sieczko.tomasz@gmail.com>
2020-08-12 16:52:53 -0700

    Chapter 14

---
+++ csharp_8_and_dotnet_core_3/Chapter14/PracticalApps/NorthwindEntitiesLib/Shipper.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace NorthwindEntitiesLib
+{
+	public class Shipper
+	{
+		public int ShipperID { get; set; }
+		public string ShipperName { get; set; }
+		public string Phone { get; set; }
+
+		// related entities
+		public ICollection<Order> Orders { get; set; }
+
+	}
+}
